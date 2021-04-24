612104cc6b8fc3bb158e024d85b45e5568ab7bf6
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-04-23 21:00:24 -0300

    Project finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Currency Converter</h1>
+    <form action>
+      <label>
+        Value (R$)
+        <InputText v-model="currentValue" placeholder="0" type="number" step="0.1" min="0" />
+      </label>
+      <label>
+        R$ / US$
+        <InputText
+          v-model="currencyConvertValue"
+          placeholder="0"
+          type="number"
+          step="0.01"
+          min="0"
+        />
+      </label>
+      <div id="divButtons">
+        <Button
+          label="Clear"
+          @click="clear()"
+          class="p-button-secondary"
+          :disabled="!currentValue && !currencyConvertValue"
+        />
+        <Button
+          label="Calculate"
+          @click="calculate()"
+          :disabled="!currentValue || !currencyConvertValue"
+        />
+      </div>
+    </form>
+    <article v-show="resultValue && currentValue">
+      <h2>Result</h2>
+      <p>
+        R$ {{parseInt(currentValue).toFixed(2)}} =>
+        <span id="result">US$ {{resultValue.toFixed(2)}}</span>
+      </p>
+    </article>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+document.title = "Currency Converter";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      currentValue: null,
+      currencyConvertValue: 5.48,
+      resultValue: 0
+    };
+  },
+  methods: {
+    calculate() {
+      this.$data.resultValue =
+        this.$data.currentValue / this.$data.currencyConvertValue;
+    },
+    clear() {
+      this.$data.currentValue = null;
+      this.$data.currencyConvertValue = null;
+      this.$data.resultValue = 0;
+    }
+  },
+  watch: {
+    currentValue() {
+      this.$data.resultValue = 0;
+    },
+    currencyConvertValue() {
+      this.$data.resultValue = 0;
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +80,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+form {
+  width: max-content;
+  margin: auto;
+}
+label {
+  display: block;
+  text-align: right;
+  padding-right: 25px;
+  margin: 10px auto;
+}
+label > input {
+  margin-left: 5px !important;
+}
+#divButtons > button {
+  width: 100px;
+  margin: 10px 30px !important;
+}
+span#result {
+  font-weight: bold;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+//Components
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+//Style
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.mount('#app');
